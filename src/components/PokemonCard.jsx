a85572ed107d8a4bc65da231feb46984894812a5
Calypso salé <calypso45000@gmail.com>
2023-10-11 12:21:45 +0200

    PokemonCard component added

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PokemonCard = () => {
+  return (
+    <figure>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="Bulbasaur"
+      />
+      <figcaption>Bulbasaur</figcaption>
+    </figure>
+  );
+};
+
+export default PokemonCard;
