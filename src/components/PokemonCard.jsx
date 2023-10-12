4f478081bebc7b5c536037b4c4562dadadda4e2a
Calypso salé <calypso45000@gmail.com>
2023-10-12 09:55:56 +0200

    Making PokemonCard dynamic

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
 const PokemonCard = () => {
+  const pokemonList = [
+    {
+      name: "Bulbasaur", 
+      imgSrc: 
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name:"Mew",
+    },
+  ];
+
+  const pokemon = pokemonList[0];
+
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+      <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 };
+
 
 export default PokemonCard;
