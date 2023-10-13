bfe2b8092651a25f9bd05014396cd647c5f0603f
Calypso salé <calypso45000@gmail.com>
2023-10-13 12:18:55 +0200

    Add state and navigation buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import PokemonCard from './components/PokemonCard';
 import './App.css'
+import PokemonCard from './components/PokemonCard';
+import React, { useState } from 'react';
 
-const App = () => { 
 const pokemonList = [
   {
-    name: "Bulbasaur", 
-    imgSrc: 
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name:"Mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-const pokemon = pokemonList[0];
-return (
-  <div>
-    <PokemonCard pokemon={pokemon} />
-  </div>
+function App() {
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClickPrevious = () => {
+    setPokemonIndex(pokemonIndex - 1)}
+
+  const handleClickNext = () => {
+      setPokemonIndex(pokemonIndex + 1)}
+
+  const pokemon = pokemonList[pokemonIndex];
+
+  return (
+    <>
+    <div>
+      <PokemonCard pokemon={pokemon}/>
+
+      {pokemonIndex > 0 && <button onClick={handleClickPrevious}>Previous</button>}
+      {pokemonIndex < pokemonList.length - 1 && <button onClick={handleClickNext}>Next</button> }
+    </div>
+    </>
   );
-};
+}
 
 
-export default App
+
+export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 const PokemonCard = (props) => {
   const {pokemon} = props ;
   return (
